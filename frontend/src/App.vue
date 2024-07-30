b930db25c041f3eed5fe285d9be4628863b1f227
Stenford <varonapika@gmail.com>
2024-07-30 23:43:54 +0200

    0.1.0: Base A base for the frontend has been created, a very simple site layout, a Dockerfile for the frontend (I've finally mastered Docker). Git repository created

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="gradient-bg flex-col h-[100vh]">
+    <Header></Header>
+  </div>
+
+</template>
+
+<script>
+import {defineComponent} from "vue";
+import Header from "./components/Header.vue";
+
+export default defineComponent({
+  components: {Header}
+})
+</script>
+<style scoped>
+.gradient-bg {
+  background: linear-gradient(135deg, #9CF4FF 0%, #FFFFFF 100%);
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="p-2">
+    <a href="#"><img src="@/assets/imgs/logo.png" alt="Logo" class="w-[60px] hover:scale-105 hover:opacity-95 duration-200 cursor-pointer"></a>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+
+}
+
+</script>
